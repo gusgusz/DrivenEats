d5e2e2cb389e1aaf9cc10a65e07836b3eaf22e97
Gustavo <gustavoguimaraescamps@gmail.com>
2022-04-04 16:52:49 -0300

    layout e todos selecionados

--- projeto3-driveneats/scripts.js
+++ projeto3-driveneats/scripts.js
@@ -1,13 +1,71 @@
+let preçoComida = null;
+let preçoSobremesa = null;
+let preçoBebida = null;
+let selecionadoComida;
+let selecionadoBebida;
+let selecionadoSobremesa;
+
+
 function escolherPrato (comida){
-    console.log(comida);
-    const selecionado = document.querySelector(".selecionado");
+    selecionadoComida= document.querySelector(".selecionado-comida");
 
-
-    if(selecionado !== null){
-        selecionado.classList.remove("selecionado");
+ 
+    if(selecionadoComida !== null){
+      selecionadoComida.classList.remove("selecionado-comida");
         
     }
-    comida.classList.add("selecionado");
+    comida.classList.add("selecionado-comida");
+    preçoComida = document
+    .querySelector(".selecionado-comida")
+    .querySelector("span").innerHTML;
+    console.log(preçoComida);
+    botaoVerde();
+    
+}
+
+function escolherBebida (bebida){
+    
+    selecionadoBebida = document.querySelector(".selecionado-bebida");
     
 
+
+    if(selecionadoBebida !== null){
+        selecionadoBebida.classList.remove("selecionado-bebida");
+        
+    }
+    bebida.classList.add("selecionado-bebida");
+    preçoBebida = document
+    .querySelector(".selecionado-bebida")
+    .querySelector("span").innerHTML;
+    botaoVerde();
+    
+
+} 
+
+function escolherSobremesa (sobremesa){
+    
+    selecionadoSobremesa = document.querySelector(".selecionado-sobremesa");
+    
+
+
+    if(selecionadoSobremesa !== null){
+        selecionadoSobremesa.classList.remove("selecionado-sobremesa");
+        
+    }
+    sobremesa.classList.add("selecionado-sobremesa");
+    preçoSobremesa = document
+    .querySelector(".selecionado-sobremesa")
+    .querySelector("span").innerHTML;
+    botaoVerde();
+}
+    
+
+function botaoVerde () {
+
+if((preçoComida !== null) && (preçoBebida !== null) && (preçoSobremesa !== null)){
+    let botao = document.querySelector("button");
+    
+    botao.classList.add("selecionado-tudo");
+    
+}
 }
